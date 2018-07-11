9f234ca1c85d505e609dfc6361dafba5127156a0
Austin Condict <34462677+VicerExciser@users.noreply.github.com>
2018-07-10 23:45:07 -0400

    Update QuizDB.java
    Task Card 1 completed

--- src/main/java/edu/gatech/quiz/data/QuizDB.java
+++ src/main/java/edu/gatech/quiz/data/QuizDB.java
@@ -15,10 +15,9 @@ public class QuizDB {
     public List<String> getQuestionCategories() {
         Connection c = getConnection();
         List<String> categories = new ArrayList<>();
+        
         PreparedStatement stmt = null;
         ResultSet rs = null;
-
-//        String query = "SELECT title FROM categories";
         String catQuery = "SELECT * FROM categories";
         try {
             stmt = c.prepareStatement(catQuery);
@@ -27,7 +26,6 @@ public class QuizDB {
             while (rs.next()) {
                 categories.add(rs.getString("title"));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -40,40 +38,22 @@ public class QuizDB {
     public List<Question> getCategoryQuestions(String category) {
         Connection c = getConnection();
         List<Question> questions = new ArrayList<>();
-
-
-//        List<String[]> qDataSets = new ArrayList<>();
-//        String[] qData = null;
+        
         PreparedStatement stmt = null;
         ResultSet rs = null;
-
-        int catId;
-
-//        int answId;
-
-        String catIdQuery = "SELECT id FROM categories WHERE title=?";
         String questQuery = "SELECT id, body, explanation FROM questions " +
-                "WHERE category_id=?";
+                "WHERE category_id = (SELECT id FROM categories WHERE title=?)";
         String answQuery = "SELECT correct, body FROM answers WHERE " +
                 "question_id=?";
-
         try {
-            stmt = c.prepareStatement(catIdQuery);
-            stmt.setString(1, category);
-            catId = stmt.executeQuery().getInt("id");
-
-//            qData = new String[3];
             stmt = c.prepareStatement(questQuery);
-            stmt.setInt(1, catId);
+            stmt.setString(1, category);
             rs = stmt.executeQuery();
-            // boolean hasMoreSets = stmt.execute();
-            // while (hasMoreSets) { rs = stmt.getResultSet(); ... }
+            
             while (rs.next()) {
                 int questId = rs.getInt("id");
                 String qBody = rs.getString("body");
-//                qData[0] = rs.getString("body");
                 String qExpl  = rs.getString("explanation");
-//                qData[2] = rs.getString("explanation");
 
                 List <Option> answers = new ArrayList<>();
                 PreparedStatement subStmt = c.prepareStatement(answQuery);
@@ -85,15 +65,19 @@ public class QuizDB {
                     boolean aCorrect = subRs.getBoolean("correct");
                     answers.add(new Option(aBody, aCorrect));
                 }
+                subRs.close();
+                subStmt.close();
 
                 Question q = new Question(category, qBody, answers, qExpl);
                 questions.add(q);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             tearDown(rs, stmt, c);
+        }
+        if (questions.isEmpty()) {
+        	questions = null;
         }
 
         return questions;
@@ -101,20 +85,23 @@ public class QuizDB {
 
     private void tearDown(ResultSet rs, PreparedStatement stmt, Connection c) {
         try {
-            if (rs != null)
+            if (rs != null) {
                 rs.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
-            if (stmt != null)
+            if (stmt != null) {
                 stmt.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
-            if (c != null)
+            if (c != null) {
                 c.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
