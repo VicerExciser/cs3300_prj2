97b53ef1d2918556183b267d897b6f36d80c2e65
Zach Edmands <zedmands@zoho.com>
2018-07-12 18:08:42 -0400

    TaskCards finished. QuizExtraTests stub created.

---
+++ src/test/java/QuizExtraTests.java
@@ -0,0 +1,12 @@
+import edu.gatech.quiz.data.QuizDB;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuizExtraTests {
+    private QuizDB db;
+
+    @Before
+    public void setUp() {
+        db = new QuizDB();
+    }
+}
